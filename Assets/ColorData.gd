381aea9698bc0b1704aa5cc5e89d9ff3e23aa3cb
Neno <nebojsa.kovacevik@gmail.com>
2024-01-13 20:09:45 +0100

    implement color - sound pairing functionality

--- Assets/ColorData.gd
+++ Assets/ColorData.gd
@@ -1,15 +1,12 @@
 const RED = Color(1, 0, 0, 1.0)
 const ORANGE = Color(1, 0.45, 0, 1.0)
 const YELLOW = Color(1, 1, 0, 1.0)
-
 const GREEN = Color(0.3, 1, 0, 1.0)
 const JADE = Color(0, 1, 0.4, 1.0)
 const TURQUOISE = Color(0, 1, 1, 1.0)
-
 const BABY_BLUE = Color(0, 0.6, 1, 1.0)
 const BLUE = Color(0.1, 0.1, 1, 1.0)
 const PURPLE = Color(0.75, 0, 1, 1.0)
-
 const PINK = Color(1, 0, 0.9, 1.0)
 const SALMON = Color(1, 0, 0.45, 1.0)
 
--- Game.gd
+++ Game.gd
@@ -1,16 +1,57 @@
 extends Node2D
-const AudioData = preload("res://Assets/AudioData.gd")
 # Called when the node enters the scene tree for the first time.
 const BUTTON = preload("res://ColorButton.tscn")
-func _ready():
-	var new_button = BUTTON.instantiate()
-	new_button.position = Vector2(250, 250)
-	add_child(new_button)
+
+const AudioData = preload("res://Assets/AudioData.gd")
+const ColorData = preload("res://Assets/ColorData.gd")
+
+#const colors = [green, jade, turquoise, baby_blue, blue, purple, pink, salmon]
+var colors: Array = ColorData.DATA
+const sounds: Dictionary = AudioData.DATA
+var index = 0
+var pairs = {
 	
-	$AudioStreamPlayer.stream = AudioData.DATA[randi_range(0, AudioData.DATA.size() - 1)]
-	$AudioStreamPlayer.playing = true
-	pass # Replace with function body.
+}
+
+func _ready():
+	
+	generate_color_pairs()
+	var i = 0
+	for key in pairs:
+		var new_button = BUTTON.instantiate()
+	
+		new_button.position = (Vector2((250 * i), 250))
+		new_button.get_node("Color").color = pairs[key].color
+		new_button.get_node("Sound").stream = pairs[key].sound
+		
+		add_child(new_button)
+		i += 1
+		
+	
+	#$AudioStreamPlayer.stream = AudioData.DATA.values()[randi_range(0, AudioData.DATA.size() - 1)]
+	#$AudioStreamPlayer.playing = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func generate_color_pairs():
+	var index = 0
+	for i in range(3):
+		#print(i, colors[i], sounds.keys()[i])
+		pairs[sounds.keys()[index]] = {
+			sound = sounds.values()[index],
+			#color = colors[index],
+			color = mix_color([colors[index], colors[index + 1]]),
+		}
+		index += 2
+	#print(pairs)
+
+func mix_color(colors: Array[Color]) -> Color:
+	# Mix two or more Colors
+	var new_color: Color
+	var color_sum: Color
+	
+	for color in colors:
+		color_sum += color
+	return color_sum / colors.size()
