214696133881dd2e5166d95d2c58d00a98490f57
Neno <nebojsa.kovacevik@gmail.com>
2024-01-13 14:00:51 +0100

    rearrange ColorButton

---
+++ ColorButton.gd
@@ -0,0 +1,48 @@
+extends MarginContainer
+
+@onready var button_color = $Color
+
+const red = Color(1, 0, 0, 1.0)
+const orange = Color(1, 0.45, 0, 1.0)
+const yellow = Color(1, 1, 0, 1.0)
+
+const green = Color(0.3, 1, 0, 1.0)
+const jade = Color(0, 1, 0.4, 1.0)
+const turquoise = Color(0, 1, 1, 1.0)
+
+const baby_blue = Color(0, 0.6, 1, 1.0)
+const blue = Color(0.1, 0.1, 1, 1.0)
+const purple = Color(0.75, 0, 1, 1.0)
+
+const pink = Color(1, 0, 0.9, 1.0)
+const salmon = Color(1, 0, 0.45, 1.0)
+
+
+const colors = [green, jade, turquoise, baby_blue, blue, purple, pink, salmon]
+const test_colors = [purple, blue]
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#var color = Color(1, 0, 0)
+	button_color.color = colors[randi_range(0, colors.size() - 1)]
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func mix_color(colors: Array[Color]) -> Color:
+	var new_color: Color
+	var color_sum: Color
+	
+	for color in colors:
+		color_sum += color
+	return color_sum / colors.size()
+
+
+func _on_button_button_up():
+	# Change color on button click
+	button_color.set_color(test_colors[randi_range(0, test_colors.size() - 1)])
--- Game.gd
+++ Game.gd
@@ -1,17 +1,15 @@
 extends Node2D
-const AudioData = preload("res://AudioData.gd")
-const GRAND_PIANO_A_ = preload("res://audio/piano/grand_piano_a#.wav")
+const AudioData = preload("res://audio/AudioData.gd")
 # Called when the node enters the scene tree for the first time.
-const BUTTON = preload("res://Button.tscn")
+const BUTTON = preload("res://ColorButton.tscn")
 func _ready():
 	var new_button = BUTTON.instantiate()
 	new_button.position = Vector2(250, 250)
 	add_child(new_button)
-	#$AudioStreamPlayer.stream = AudioData.GRAND_PIANO_A
+	
 	$AudioStreamPlayer.stream = AudioData.DATA[randi_range(0, AudioData.DATA.size() - 1)]
 	$AudioStreamPlayer.playing = true
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
