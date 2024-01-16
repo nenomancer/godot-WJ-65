94d5541c002d477b4155aabb1ba8ca5b0cb0d701
Neno <nebojsa.kovacevik@gmail.com>
2024-01-16 15:43:40 +0100

    refactor code, add timers

--- Game.gd
+++ Game.gd
@@ -4,6 +4,12 @@ const BUTTON = preload("res://button.tscn")
 const audio_data = preload("res://Assets/audio_data.gd")
 const color_data = preload("res://Assets/color_data.gd")
 
+const result_text = {
+	none = "",
+	correct = "That's the correct answer",
+	incorrect = "That's the wrong answer",
+}
+
 @onready var background_correct = $"../BackgroundCorrect"
 @onready var points_label = $"../Points"
 var points: int = 0
@@ -11,8 +17,9 @@ var stages: int = 3
 var stage_index: int = 0
 
 #const colors = [green, jade, turquoise, baby_blue, blue, purple, pink, salmon]
-var colors: Array = color_data.DATA
+var colors: Array = color_data.DATA.duplicate(true)
 const sounds: Dictionary = audio_data.DATA
+
 # VALJDA KJE TREBA ARRAY OD VEKJE GENERIRANI KOOPCHINAJ
 var loaded_buttons: Array
 var generated_buttons: Array
@@ -27,6 +34,7 @@ var sound_color_pairs = {
 var correct_index: int
 var correct_color: Color
 var correct_sound: AudioStreamWAV
+var number_of_options = 3
 
 func _ready():
 	generate_color_pairs(12, 1)
@@ -37,81 +45,106 @@ func _ready():
 	if loaded_buttons.size() > 0:
 		load_buttons()	
 		
-	# Use to randomly generate buttons
-	#generate_buttons(3)
-	#var i = 0
-	refresh_scene()
+	#next_question(number_of_options)
+	generate_buttons(12, true)
+	enable_buttons(true)
+
+func generate_color_pairs(note_range: int = 3, semitone_skip: int = 4):
+	colors.shuffle()
+	var index = 0
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	for i in note_range:
+		sound_color_pairs[sounds.keys()[index]] = {
+			note = sounds.keys()[index],
+			sound = sounds.values()[index],
+			color = colors[index],
+		}
+		index = (index + semitone_skip) % sounds.size() # Every OTHER note (in this case: C, E, G)
+
+func init_stage():
+	enable_buttons(false)
+	await get_tree().create_timer(2).timeout
+	$MainSound.playing = true
+	await get_tree().create_timer(2).timeout
+	enable_buttons(true)
+
+
+func next_question(number_of_questions: int):
+	await get_tree().create_timer(2).timeout
 	
-func refresh_scene():
+	background_correct.visible = false
 	if stage_index < stages:
 		for child in $Colors.get_children():
 			$Colors.remove_child(child)
-		generate_buttons(3)
+		generate_buttons(number_of_options)
 		stage_index += 1
+		init_stage()
 	else:
 		print("NO MORE STAGES WTF")
 	
-		#show_question()
-		
+func enable_buttons(enable: bool = true):
+	for child in $Colors.get_children():
+		if enable:
+			child.get_node("Container/Button").disabled = false
+		else:
+			child.get_node("Container/Button").disabled = true
+
 func update_points(point_amount: int = 0):
 	points += point_amount
 	points_label.text = "Points: " + str(points)
+	
+	# MAYBE ADD SOUND EFFECTS FOR EVERYTHING
+	# material.set_shader_parameter("some_value", some_value)
+	# material.set_shader_parameter("colors", [Vector3(1, 0, 0), Vector3(0, 1, 0), Vector3(0, 0, 1)])
 
 func set_correct_answer(button: MarginContainer):
-	correct_color = button.get_node("Color").color
-	correct_sound = button.get_node("Sound").stream
-	$CorrectNote.text = "Correct note: " + button.get_node("NoteName").text
+	correct_color = button.get_node("Container/Color").color
+	correct_sound = button.get_node("Container/Sound").stream
+	$CorrectNote.text = "Correct note: " + button.get_node("Container/NoteName").text
+	$MainSound.stream = correct_sound
 
 func load_buttons():
 	correct_index = randi_range(0, loaded_buttons.size() - 1)
 	
 	for button in loaded_buttons:
-		var target_sound = button.get_node("Sound").stream
+		var target_sound = button.get_node("Container/Sound").stream
 		button.color_clicked.connect(Callable(check_answer.bind(button)))
-		
+		button.get_node("Container/Button").disabled = true
 		for key in sound_color_pairs:
 			var audition_sound = sound_color_pairs[key].sound
 			if(audition_sound == target_sound):
-				var text_color = Color.BLACK
 				var new_color = sound_color_pairs[key].color
 				var new_text = key
+				var loaded_sound = button.get_node("Container/Sound").stream
 				button.color_hover = new_color.lightened(0.5)
-				var sound = button.get_node("Sound").stream
 				
-				button.configure_button(sound, new_color, new_text, text_color)
-				#button.get_node("Color").color = new_color
-				#button.get_node("NoteName").text = new_text
-				#button.color_value = new_color
+				button.configure_button(loaded_sound, new_color, new_text)
 				
 		if correct_index == button.get_index():
 			set_correct_answer(button)
 
-func generate_buttons(number_of_buttons: int):
+func generate_buttons(number_of_buttons: int, first_time: bool = false):
 	var i = 0
+	if first_time:
+		sound_color_pairs.values().shuffle()
 	correct_index = randi_range(0, number_of_buttons - 1)
 	print("CORRECT INDEX: ", correct_index)
 	
 	while i < number_of_buttons:
-		# 0
-		# sound_color_pairs[0]
 		var indexx = randi_range(0, sound_color_pairs.values().size() - 1)
 		
 		var new_button = BUTTON.instantiate()
 		
-		var text_color: Color = Color.BLACK
-		var new_color: Color = sound_color_pairs.values()[indexx].color
-		var new_sound: AudioStreamWAV = sound_color_pairs.values()[indexx].sound
-		var new_label: String = sound_color_pairs.values()[indexx].note
+		var new_color: Color = sound_color_pairs.values()[i].color
+		var new_sound: AudioStreamWAV = sound_color_pairs.values()[i].sound
+		var new_label: String = sound_color_pairs.values()[i].note
 		
 		new_button.color_value = new_color
 		new_button.color_hover = new_color.lightened(0.5)
 		new_button.color_clicked.connect(Callable(check_answer).bind(new_button))
-		
-		new_button.configure_button(new_sound, new_color, new_label, text_color)
+		new_button.configure_button(new_sound, new_color, new_label)
+		new_button.get_node("Container/Button").disabled = true
+		print(new_button.get_node("Container/Button"))
 		
 		if correct_index == i: 
 			set_correct_answer(new_button)
@@ -125,26 +158,24 @@ func generate_buttons(number_of_buttons: int):
 		i += 1
 
 func check_answer(button):
-	print('Button: ', button, ', correct sound: ', correct_sound, ', button sound: ', button.get_node("Sound").stream)
-	if correct_sound == button.get_node("Sound").stream:
+	print('Button: ', button, ', correct sound: ', correct_sound, ', button sound: ', button.get_node("Container/Sound").stream)
+	if correct_sound == button.get_node("Container/Sound").stream:
 		print("You guessed the CORRECT color")
 		background_correct.visible = true
 		update_points(10)
-		refresh_scene()
+		enable_buttons(false)
+		await get_tree().create_timer(2).timeout
+		background_correct.visible = false
+		next_question(number_of_options)
 	else:
 		print("You guessed the WRONG color")
 		background_correct.visible = false
 		update_points(-5)
+		enable_buttons(false)
+		await get_tree().create_timer(2).timeout
+		background_correct.visible = false
+		next_question(number_of_options)
 	
-func generate_color_pairs(note_range: int = 3, semitone_skip: int = 4):
-	var index = 0
-	for i in note_range:
-		sound_color_pairs[sounds.keys()[index]] = {
-			note = sounds.keys()[index],
-			sound = sounds.values()[index],
-			color = colors[index],
-		}
-		index = (index + semitone_skip) % sounds.size() # Every OTHER note (in this case: C, E, G)
 
 func mix_color(colors: Array[Color]) -> Color:
 	# Mix two or more Colors
@@ -154,5 +185,3 @@ func mix_color(colors: Array[Color]) -> Color:
 	for color in colors:
 		color_sum += color
 	return color_sum / colors.size()
-	
-
--- button.gd
+++ button.gd
@@ -1,9 +1,10 @@
 extends MarginContainer
 signal color_clicked(button)
 
-@onready var button_color = $Color.color
-@onready var button_sound = $Sound
-@onready var button_label = $NoteName
+@onready var button = $Container/Color.color
+@onready var button_sound = $Container/Sound
+@onready var button_label = $Container/NoteName
+var button_color: Color
 var is_hovered: bool = false
 
 var color_value: Color
@@ -18,15 +19,24 @@ func _ready():
 	if current_sound != null:
 		button_sound.stream = current_sound
 		#
+	
+	#print(current_sound.get_length() / 2)
+	#await get_tree().create_timer(2).timeout
+	#$Container/Color.color = Color.ANTIQUE_WHITE
 	#color_value = button_color.color
 	#color_hover = color_value.lightened(0.25)
 
-func configure_button(audio: AudioStreamWAV, fill_color: Color, text: String, text_color: Color):
-	$Sound.stream = audio
-	$Color.color = fill_color
-	$NoteName.text = text
-	$NoteName.label_settings.font_color = text_color
+func configure_button(audio: AudioStreamWAV, fill_color: Color, text: String):
+	$Container/Sound.stream = audio
+	$Container/Color.color = fill_color
+	$Container/NoteName.text = text
+	button_color = fill_color
 	color_hover = fill_color.lightened(0.5)
+	
+	# Material shit (not needed for now)
+	var shader: Shader = $Container/BGNoise.material.shader
+	var mat: ShaderMaterial = $Container/BGNoise.material
+	mat.set_shader_parameter("color", fill_color)
 
 func _on_button_button_up():
 	button_sound.playing = true
@@ -34,8 +44,8 @@ func _on_button_button_up():
 
 func _on_button_mouse_entered():
 	var tween = get_tree().create_tween()
-	tween.tween_property($Color, "color", button_color.lightened(0.5), 0.25)
+	tween.tween_property($Container/Color, "color", button_color.lightened(0.5), 0.25)
 
 func _on_button_mouse_exited():
 	var tween = get_tree().create_tween()
-	tween.tween_property($Color, "color", button_color, 0.25)
+	tween.tween_property($Container/Color, "color", button_color, 0.25)
